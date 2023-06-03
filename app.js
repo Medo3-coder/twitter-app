fa5c647a628fcf0a62653ea5637fdca969ed7d55
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-03 15:04:56 +0300

    Rendering some content on the web page

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const server = app.listen(port, () => console.log(`listening on port: ${port}`))
+const server = app.listen(port, () => console.log(`listening on port: ${port}`));
+
+app.get('/' , (req, res,next) => {
+    res.status(200).send('Welcome');
+})
