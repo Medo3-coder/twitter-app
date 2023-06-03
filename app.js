b29138e3f3768412a3c96ed67d0873a0c09854ba
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-03 16:49:58 +0300

    Middleware Redirecting the user if not logged in

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const login = require('./middleware/requireLogin')
 const port = 3000;
 const server = app.listen(port, () => console.log(`listening on port: ${port}`));
 
 app.set('view engine', 'pug');
-app.get('/' , (req, res,next) => {
+app.get('/', login, (req, res, next) => {
 
-   var payload = {
-      pageTitle: "home",
-   }
-    res.status(200).render('home' , payload);
+    var payload = {
+        pageTitle: "home",
+    }
+    res.status(200).render('home', payload);
 })
