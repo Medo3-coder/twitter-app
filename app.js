5e1814b57afba01c472ff4a012351c074b341fec
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-05 01:15:41 +0300

    Serving static files

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const login = require('./middleware/requireLogin')
 const port = 3000;
+const path = require('path');
 const server = app.listen(port, () => console.log(`listening on port: ${port}`));
 
 app.set('view engine', 'pug');
 app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //routes 
 // Routes
