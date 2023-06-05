d4121f63dad37afee12079e4b24dba19521acd12
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-06 01:07:05 +0300

    Creating the User Schema (mongoose)

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const  usersSchema = new mongoose.Schema({
+     firstName : {type: 'string', required: true , trim: true},
+     lastName : {type: 'string', required: true , trim: true},
+     username  : {type: 'string', required: true , trim: true , unique: true},
+     email : {type: 'string', required: true , trim: true , unique: true},
+     password   : {type:String,require:true},
+     profilePic : {type: String, default:"/images/profilePic.png"},
+    });
+
+    var User = mongoose.model('User', usersSchema);
