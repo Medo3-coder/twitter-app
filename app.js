4cc03f0ec2ac1d4a78e123cd62de8c84ef648df8
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-06-05 16:52:05 +0300

    Connecting to the database

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const login = require('./middleware/requireLogin')
 const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://medo:LoooooL2244565@twitterclonecluster.or0hxwd.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('database connection established')
+    })
+    .catch((err) => {
+        console.log('database connection error' + err.message)
+    })
 const server = app.listen(port, () => console.log(`listening on port: ${port}`));
 
 app.set('view engine', 'pug');
@@ -16,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const loginRoute = require('./routes/loginRoute');
 const registerRoute = require('./routes/registerRoute');
 
-app.use('/' ,loginRoute )
+app.use('/', loginRoute)
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 
